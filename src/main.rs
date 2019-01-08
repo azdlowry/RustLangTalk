e1bb0b1ae8d7b17d1e79f0de5bc718b281c93772
Andy Lowry <andy.lowry@moonpig.com>
2019-01-08 19:38:34 +0000

    Basic Cards

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
+#[derive(Debug)]
+pub enum Suit {
+    Diamonds,
+    Hearts,
+    Clubs,
+    Spades,
+}
+
+#[derive(Debug)]
+pub enum Rank {
+    A,
+    R2,
+    R3,
+    R4,
+    R5,
+    R6,
+    R7,
+    R8,
+    R9,
+    R10,
+    J,
+    Q,
+    K,
+}
+
+#[derive(Debug)]
+pub struct Card(Suit, Rank);
+
 fn main() {
-    let text = "Hello, world!";
-    println!("{}", text);
-
-    let x = 24;
-    println!("{}", x);
-    let y = 34;
-    println!("{}", y);
-
-    let z = x + y;
-    println!("{}", z);
+    let suit = Suit::Diamonds;
+    let rank = Rank::R4;
+    let card = Card(suit, rank);
+    println!("{:?}", card);
 }
